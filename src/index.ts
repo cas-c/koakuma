ba0de5fe63457668b10f9b58995414bfcf239fbf
cassandra <cassie@medal.tv>
2022-01-20 00:20:34 -0700

    dev experience tools for running tsc and nodemon at the same time ^o^

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hi');
+import { Client, Intents } from "discord.js";
+const config = require("../config.json");
+
+const Koakuma = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS
+    ]
+});
+
+Koakuma.once('ready', (client: Client) => {
+    console.log("ready");
+})
+
+Koakuma.login(config.token);
