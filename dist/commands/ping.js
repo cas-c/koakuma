33a9c4a8ed4c863fa29886762cf0bd2d2ce8802a
cassie <hi@cassie.fish>
2023-06-18 12:36:15 -0600

    initial rewrite, working tsc

--- dist/commands/ping.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ping = (message) => {
-    return message.reply("pong");
-};
-exports.default = ping;
-//# sourceMappingURL=ping.js.map
